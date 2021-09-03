0b37c89be648eb6036a0650eefda75688d953d33
shannon_p <snp1284kasey@aol.com>
2021-09-03 12:22:24 -0400

    modularize the application

---
+++ db/connection.js
@@ -0,0 +1,26 @@
+const mysql = require("mysql2");
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        // Your MySQL username,
+        user: "root",
+        // Your MySQL password,
+        password: "xM9VBi8@HrD7",
+        database: "election"
+    },
+    console.log("Connected to the election database.")
+);
+
+
+
+
+
+
+
+
+
+
+
+module.exports = db;
